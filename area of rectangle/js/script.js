71aaebb957e8bfac832103e705225d358aea590b
AdelinaWood <CarpeDiemAdel@gmail.com>
2022-09-05 09:52:50 +0300

    area of rect in a coordinate

---
+++ area of rectangle/js/script.js
@@ -0,0 +1,9 @@
+let x1 = +prompt('Enter the first x1: ',' ');
+let x2 = +prompt('Enter the first x2: ',' ');
+
+let y1 = +prompt('Enter the first y1: ',' ');
+let y2 = +prompt('Enter the first x2: ',' ');
+
+let s = Math.abs((x2 - x1) * (y2 - y1));
+
+ console.log(s);
